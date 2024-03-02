70613b0287b703ab026a0cf32f4ad153bea35f27
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2024-03-02 18:23:19 +0300

    Добавьте файлы проекта.

---
+++ lesson8_task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lesson8_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:\\Programs\\MyLabsITMO\\C#course";
+            Console.WriteLine("Начинаем поиск в деректории: {0}", path);
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs)
+            {
+                string[] dirsInside = Directory.GetDirectories(dir);
+                Console.WriteLine("следующая директория внутри: {0}", dir);
+                Console.WriteLine("файлы в данной директории");
+                string[] files = Directory.GetFiles(dir);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }   
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson8_task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lesson8_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.IO.Directory.GetCurrentDirectory();
+            Random rnd = new Random();
+            long sum = 0;
+            string parentPath = Directory.GetParent(Directory.GetParent(path).FullName).FullName;
+            Console.WriteLine(parentPath);
+
+            string filePath = String.Concat(parentPath, "\\","textFile.txt");
+            Console.WriteLine("filePath {0}",parentPath);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                for (int i=0; i<10; i++)
+                {
+                    sw.WriteLine(rnd.Next());
+                }
+            }
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    int number = Convert.ToInt32(line);
+                    sum += number;
+                    line = sr.ReadLine();
+                }
+
+            }
+
+            Console.WriteLine("Сумма чисел {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
